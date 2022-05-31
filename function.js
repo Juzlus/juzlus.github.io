d5c899ca3be1df0f12f9c71085ff59f43dcfc1d7
Juzlus <juzlus.biznes@gmail.com>
2022-05-31 18:23:08 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+﻿function CheckWidth() {
+    if( ( window.innerWidth <= 1200 ) || ( window.innerHeight <= 600 ) )
+        document.getElementsByClassName("noSupport")[0].style.display = "block";
+    else {
+        document.querySelector("header").style.display = "block";
+        document.querySelector("main").style.display = "block";
+        document.querySelector("footer").style.display = "block";
+    }
+}
